92fb09b83db195f06b283a8c1e2b88769e8326da
chinakr <chnakr@gmail.com>
2013-01-05 08:32:10 +0800

    Finish urls and views skeleton for admin.

--- bdpolls/admin/views.py
+++ bdpolls/admin/views.py
@@ -1,1 +1,63 @@
-# Create your views here.
+#-*- coding: utf-8 -*-
+
+from django.shortcuts import render, redirect
+
+def listing(request):
+    """问卷列表(问卷管理)"""
+
+    pass
+
+def add(request):
+    """添加问卷"""
+
+    pass
+
+def edit(request, questionnaire_id):
+    """修改问卷"""
+
+    pass
+
+def delete(request, questionnaire_id):
+    """删除问卷"""
+
+    pass
+
+def add_question(request):
+    """添加问题"""
+
+    pass
+
+def edit_question(request, question_id):
+    """修改问题"""
+
+    pass
+
+def delete_question(request, question_id):
+    """删除问题"""
+
+    pass
+
+def add_option(request):
+    """添加选项"""
+
+    pass
+
+def edit_option(request, option_id):
+    """修改选项"""
+
+    pass
+
+def delete_option(request, option_id):
+    """删除选项"""
+
+    pass
+
+def view_report(request, report_id):
+    """查看报表"""
+
+    pass
+
+def update_report(request, report_id):
+    """更新报表"""
+
+    pass
--- bdpolls/bdpolls/urls.py
+++ bdpolls/bdpolls/urls.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -14,4 +16,25 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    # 管理后台
+    ## 问卷管理
+    url(r'admin/$', 'admin.views.listing'),    # 首页
+    url(r'admin/add/$', 'admin.views.add'),    # 添加问卷
+    url(r'admin/list/$', 'admin.views.listing'),    # 问卷列表(问卷管理)
+    url(r'admin/edit/(\d+)/$', 'admin.views.edit'),    # 修改问卷
+    url(r'admin/delete/(\d+)/$', 'admin.views.delete'),    # 删除问卷
+    ## 问题管理
+    url(r'admin/(\d+)/add/$', 'admin.views.add_question'),    # 添加问题
+    url(r'admin/q(\d+)/edit/$', 'admin.views.edit_question'),    # 修改问题
+    url(r'admin/q(\d+)/delete/$', 'admin.views.delete_question'),    # 删除问题
+    ## 选项管理
+    url(r'admin/q(\d+)/add/', 'admin.views.add_option'),    # 添加选项
+    url(r'admin/o(\d+)/add/', 'admin.views.edit_option'),    # 修改选项
+    url(r'admin/o(\d+)/delete/', 'admin.views.delete_option'),    # 删除选项
+    ## 报表管理
+    url(r'admin/(\d+)/report/$', 'admin.views.view_report'),    # 查看报表
+    url(r'admin/(\d+)/report/update/$', 'admin.views.update_report'),    # 更新报表
+
+
 )
